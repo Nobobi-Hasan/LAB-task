20690e939591e87171e0c00e4ec89df540584f77
Nobobi <shatin.hasan73@gmail.com>
2020-10-19 10:43:42 +0600

    L 1 2

---
+++ lab2/lab2/lab2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 2");
+            Console.WriteLine("");
+
+            //ques 1---------------------------------------
+
+            /*Console.WriteLine("Even number from 0 to 30: ");
+            for (int i = 0; i <= 30; i++)
+            {
+                if(i%2==0)
+                {
+                    Console.Write(i+(" "));
+                }
+                
+            }*/
+
+
+            //ques 2---------------------------------------
+
+            /*for (int i = 1; i <=4; i++)
+            {
+                for (int j = 1;  j<= 3; j++)
+                {
+                    Console.Write("XO");
+
+                }
+                Console.WriteLine("");
+            }*/
+
+            //ques 3---------------------------------------
+
+            /*for (int i = 1; i <= 5; i++)
+            {
+                for (int j = i; j < i + 5; j++)
+                {
+                    Console.Write(j);
+
+                }
+            }*/
+
+            //ques 4---------------------------------------
+
+            /*int a=0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    a =a + i;
+
+                }
+
+            }
+            Console.WriteLine("Sum of odd:"+ a);
+
+            int b = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    b = b + i;
+
+                }
+
+            }
+            Console.WriteLine("Sum of Even:" + b);*/
+
+            //ques 5---------------------------------------
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = i; j < i + 5; j++)
+                {
+                    Console.Write(j);
+
+                }
+            }
+
+            //ques 6---------------------------------------
+
+        }
+    }
+}
