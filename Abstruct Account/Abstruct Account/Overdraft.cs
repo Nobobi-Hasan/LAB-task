b5d21fc740c5436f4ab499ffb2c1d517d42a98c0
Nobobi <shatin.hasan73@gmail.com>
2020-11-23 10:59:21 +0600

    Abstruction

--- Abstruct Account/Abstruct Account/Overdraft.cs
+++ Abstruct Account/Abstruct Account/Overdraft.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace Abstruct_Account
 {
-    class Overdraft
+    class Overdraft:Account
     {
+        private double overdraftLimit;
+
+        public double OverdraftLimit
+        {
+            get { return overdraftLimit; }
+            set { overdraftLimit = value; }
+        }
+
+
+        public Overdraft()
+        {
+
+        }
+
+        public Overdraft(string accName, string accNo, double balance, double overdraftLimit) : base(accName, accNo, balance)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        public override void Withdraw(double amount)
+        {
+
+            if (amount <= Balance+overdraftLimit)
+            {
+                Balance -= amount;
+
+            }
+
+        }
+
     }
 }
