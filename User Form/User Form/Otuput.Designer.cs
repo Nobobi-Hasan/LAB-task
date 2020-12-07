016904bb3a66298097b8857e36adb812aea10d84
Nobobi <shatin.hasan73@gmail.com>
2020-12-07 10:59:01 +0600

    User Form

---
+++ User Form/User Form/Otuput.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace User_Form
+{
+    partial class Otuput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelReg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 617);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // labelReg
+            // 
+            this.labelReg.AutoSize = true;
+            this.labelReg.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReg.Location = new System.Drawing.Point(263, 34);
+            this.labelReg.Name = "labelReg";
+            this.labelReg.Size = new System.Drawing.Size(276, 33);
+            this.labelReg.TabIndex = 2;
+            this.labelReg.Text = "User Information";
+            // 
+            // Otuput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.labelReg);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Otuput";
+            this.Text = "Otuput";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelReg;
+    }
+}
---
+++ User Form/User Form/User management.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace User_Form
+{
+    partial class User_management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEditUser = new System.Windows.Forms.Button();
+            this.buttonAddUser = new System.Windows.Forms.Button();
+            this.labelReg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEditUser
+            // 
+            this.buttonEditUser.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonEditUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditUser.Location = new System.Drawing.Point(249, 128);
+            this.buttonEditUser.Name = "buttonEditUser";
+            this.buttonEditUser.Size = new System.Drawing.Size(133, 47);
+            this.buttonEditUser.TabIndex = 43;
+            this.buttonEditUser.Text = "Edit User";
+            this.buttonEditUser.UseVisualStyleBackColor = false;
+            // 
+            // buttonAddUser
+            // 
+            this.buttonAddUser.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonAddUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddUser.Location = new System.Drawing.Point(60, 128);
+            this.buttonAddUser.Name = "buttonAddUser";
+            this.buttonAddUser.Size = new System.Drawing.Size(133, 47);
+            this.buttonAddUser.TabIndex = 44;
+            this.buttonAddUser.Text = "Add User";
+            this.buttonAddUser.UseVisualStyleBackColor = false;
+            // 
+            // labelReg
+            // 
+            this.labelReg.AutoSize = true;
+            this.labelReg.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReg.Location = new System.Drawing.Point(77, 18);
+            this.labelReg.Name = "labelReg";
+            this.labelReg.Size = new System.Drawing.Size(284, 33);
+            this.labelReg.TabIndex = 45;
+            this.labelReg.Text = "User Management";
+            // 
+            // User_management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 214);
+            this.Controls.Add(this.labelReg);
+            this.Controls.Add(this.buttonAddUser);
+            this.Controls.Add(this.buttonEditUser);
+            this.Name = "User_management";
+            this.Text = "User_management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEditUser;
+        private System.Windows.Forms.Button buttonAddUser;
+        private System.Windows.Forms.Label labelReg;
+    }
+}
